1614c955b1ef7a7674aaea5022772c8e4c151c5c
Shinnz <aisoon.timsri@gmail.com>
2020-12-09 19:15:58 +0700

    web config for queue
    web config for queue

--- src/main/java/project/queuemanagement/controllers/QueueController.java
+++ src/main/java/project/queuemanagement/controllers/QueueController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.queuemanagement.models.Queue;
 import project.queuemanagement.service.QueueService;
 
 @RestController
+@RequestMapping("/api/queue")
 public class QueueController {
 	@Autowired
 	QueueService queueService;
--- src/main/java/project/queuemanagement/security/WebSecurityConfig.java
+++ src/main/java/project/queuemanagement/security/WebSecurityConfig.java
@@ -59,8 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
-			.antMatchers("/addQueue").permitAll()
-			.antMatchers("/QueueByBusiness").permitAll()
+			.antMatchers("/api/queue/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
